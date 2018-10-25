54479a09cfe0efbaabb4b9253d8ee8916298fba9
barbudreadmon <barbudreadmon@gmail.com>
2018-10-25 08:36:17 +0200

    (libretro) Keeping old names will make transition easier...

--- jni/Android.mk
+++ jni/Android.mk
@@ -17,7 +17,7 @@ SOURCES_C   :=
 SOURCES_CXX :=
 FBA_DEFINES :=
 
-include $(LIBRETRO_DIR)/Makefile.common
+include $(ROOT_DIR)/makefile.libretro_common
 
 COMMON_FLAGS := -DUSE_SPEEDHACKS -D__LIBRETRO__ -DANDROID -DFRONTEND_SUPPORTS_RGB565 -Wno-write-strings -DLSB_FIRST $(FBA_DEFINES)
 
